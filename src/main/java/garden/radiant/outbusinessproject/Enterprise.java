ea597bffed4856b2fd983ec0e7aa7be7d4301be3
Gérald Leban <gerald.leban0@gmail.com>
2026-01-27 14:56:14 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/garden/radiant/outbusinessproject/Enterprise.java
+++ src/main/java/garden/radiant/outbusinessproject/Enterprise.java
@@ -1,10 +1,19 @@
 package garden.radiant.outbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.NotEmpty;
 
+@Entity()
 public class Enterprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     @NotEmpty()
     private String name;
     @Size(min = 10)
--- src/main/java/garden/radiant/outbusinessproject/Project.java
+++ src/main/java/garden/radiant/outbusinessproject/Project.java
@@ -1,8 +1,17 @@
 package garden.radiant.outbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 
+@Entity()
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     @NotEmpty
     private String title;
     private String description;
