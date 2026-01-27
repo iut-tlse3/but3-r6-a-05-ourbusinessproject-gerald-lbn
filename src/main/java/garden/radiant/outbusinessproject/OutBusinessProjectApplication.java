7b1266c4d6c8ae1687e6121d59b0dc70eb03022d
Gérald Leban <gerald.leban0@gmail.com>
2026-01-27 09:19:32 +0100

    feat: boostrap Java Spring JPA Application

---
+++ src/main/java/garden/radiant/outbusinessproject/OutBusinessProjectApplication.java
@@ -0,0 +1,13 @@
+package garden.radiant.outbusinessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutBusinessProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutBusinessProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/garden/radiant/outbusinessproject/OutBusinessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package garden.radiant.outbusinessproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutBusinessProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
